ef7409166dcc81254cdfc3b2c68c8eb4e48671a9
Caleb Hendren <109324308+CalebHendren@users.noreply.github.com>
2025-05-07 13:23:23 -0400

    Update main.rs
    Support for xml and tsv | simplified command line args

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,36 @@ use clap::Parser;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
-
 use FASTAParser::run;
 
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Args {
     /// Input FASTA file path
-    #[arg(short, long)]
     input: PathBuf,
 
-    /// Output format: fasta (default), json, or csv
-    #[arg(short, long, default_value = "fasta")]
-    format: String,
-
     /// Optional output file path (stdout if omitted)
-    #[arg(short, long)]
     output: Option<PathBuf>,
 }
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
+
+    let format = match &args.output {
+        Some(path) => match path.extension().and_then(|s| s.to_str()) {
+            Some("json") => "json",
+            Some("csv") => "csv",
+            Some("tsv")  => "tsv",
+            Some("xml")  => "xml",
+            _ => "csv", // default format
+        },
+        None => "csv", // default format
+    };
+
     let writer: Box<dyn Write> = match &args.output {
         Some(path) => Box::new(File::create(path)?),
         None => Box::new(std::io::stdout()),
     };
 
-    run(&args.input, &args.format, writer)
+    run(&args.input, format, writer)
 }
