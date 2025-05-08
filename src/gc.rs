4be12d1c620c46f3f7ae378aa268db60023914f4
Caleb Hendren <109324308+CalebHendren@users.noreply.github.com>
2025-05-07 21:05:58 -0400

    Updated gc.rs to support all other file types

--- src/gc.rs
+++ src/gc.rs
@@ -1,14 +1,39 @@
-use std::{env, path::Path};
-use crate::parser;
+use std::path::Path;
+use std::io;
 
-pub fn run_gc<P: AsRef<Path>>(input: P) -> std::io::Result<()> {
-    let records = parser::parse_fasta(input.as_ref())?;
+use crate::{
+    converter::{from_csv, from_json, from_tsv, from_xml},
+    parser,
+    models::Record,
+};
+
+/// Compute and print GC content from any supported file format
+pub fn run_gc<P: AsRef<Path>>(input: P) -> io::Result<()> {
+    let path = input.as_ref();
+    let ext = path.extension().and_then(|e| e.to_str()).unwrap_or("").to_lowercase();
+
+    let result: Result<Vec<Record>, io::Error> = match ext.as_str() {
+        "json" => from_json(path.to_str().unwrap())
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string())),
+        "csv" => from_csv(path.to_str().unwrap())
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string())),
+        "tsv" => from_tsv(path.to_str().unwrap())
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string())),
+        "xml" => from_xml(path.to_str().unwrap()),
+        "fasta" | "fa" | "fna" => parser::parse_fasta(path),
+        _ => {
+            return Err(io::Error::new(io::ErrorKind::InvalidInput, format!("Unsupported format: {}", ext)));
+        }
+    };
+
+    let records = result?;
+
     for rec in records {
         let seq = &rec.seq;
-        let gc_count = seq.chars().filter(|c| matches!(c, 'G'|'C'|'g'|'c')).count();
+        let gc_count = seq.chars().filter(|c| matches!(c, 'G' | 'C' | 'g' | 'c')).count();
         let gc_content = (gc_count as f64 / seq.len() as f64) * 100.0;
-
         println!(">{} - GC Content: {:.2}%", rec.id, gc_content);
     }
+
     Ok(())
 }
